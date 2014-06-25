1ff2825734537dae3f2024e4a667ba00ed0dfd29
{user} <{email}>
2014-06-26 01:16:01 +0900

    set up deploy

--- Gemfile
+++ Gemfile
@@ -2,21 +2,31 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
+gem 'rake'
+
 gem 'sass', '~> 3.2.18'
 gem 'compass'
 gem 'susy'
 gem 'compass-h5bp'
 
-# For Github Deploy
-gem "middleman-deploy"
+# For Github master commit
+gem 'middleman-deploy'
+# For Github Page
+gem 'middleman-gh-pages'
 
-gem "middleman", "~>3.3.3"
+gem 'middleman', '~>3.3.3'
+
+# Google analytics
+gem 'middleman-google-analytics'
 
 # Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
+gem 'middleman-livereload', '~> 3.1.0'
 
 # For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+gem 'wdm', '~> 0.1.0', :platforms => [:mswin, :mingw]
 
 # Windows does not come with time zone data
-gem "tzinfo-data", platforms: [:mswin, :mingw]
+gem 'tzinfo-data', platforms: [:mswin, :mingw]
+
+# for travis to github
+gem 'travis'
--- config.rb
+++ config.rb
@@ -1,13 +1,18 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+# activate :directory_indexes
+#
+set :css_dir,    'assets/css'
+set :js_dir,     'assets/js'
+set :images_dir, 'assets/img'
 
 ###
+## Compass
+####
+#
+## Susy grids in Compass
+require 'susy'
+
+##
+#
 # Page options, layouts, aliases and proxies
 ###
 
@@ -36,37 +41,47 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :google_analytics do |ga|
+    ga.tracking_id = false
+  end
+end
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+helpers do
+  # Calculate the years for a copyright
+  def copyright_years(start_year)
+    end_year =  Date.today.year
+    if start_year == end_year
+      start_year.to_s
+    else
+      start_year.to_s + '-' + end_year.to_s
+    end
+  end
+end
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
-
+  activate :minify_css
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+  # Use relative URLs
+  activate :relative_assets
 
   # Enable cache buster
   # activate :asset_hash
 
-  # Use relative URLs
-  # activate :relative_assets
-
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+end
+activate :livereload
+activate :google_analytics do |ga|
+  ga.tracking_id = 'UA-41495968-1'
+end
+
+
+activate :deploy do |deploy|
+    deploy.method = :git
+    deploy.branch = 'gh-pages'
 end
